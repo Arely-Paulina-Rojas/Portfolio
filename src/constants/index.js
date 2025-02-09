de9c559d5a9dd4a8258f24958687756a851b5138
Paulina <rojas.arelypaulina@micorreo.upp.edu.mx>
2025-02-08 18:43:49 -0600

    Updated project descriptions

--- src/constants/index.js
+++ src/constants/index.js
@@ -227,7 +227,7 @@ const projects = [
   {
     name: "Achievements Calculator",
     description:
-      "Mobile application that allows Steam users to calculate the average percentage of achievements earned per game, besides to help for estimating necessary percentage in a game for increasing the global one, providing a user-friendly and efficient solution for users.",
+      "Mobile application that allows Steam users to calculate the average game completion rate, in addition to helping estimate necessary percentage in a game for increasing the global one, providing a user-friendly and efficient solution for users.",
     tags: [
       {
         name: "Flutter",
@@ -245,17 +245,17 @@ const projects = [
     title: "Full Stack Mobile Developer",
     date: "May 2023 - June 2023",
     points: [
-      'Designed the database model.',
-      'Developed an easy-to-understand-use interfaces using Figma.',
-      'Implemented a code repository management system using GitHub. ',
-      'Developed all CRUD functions using Flutter with SQLite. ',
-      'Conducted testing and user feedback analysis eliminating bugs.'
+      'Designed and developed the database model, ensuring efficient data storage and retrieval.',
+      'Created user-friendly interfaces using Figma, making them easy to understand and use.',
+      'Implemented a code repository management system with GitHub, ensuring version control and collaboration.',
+      'Developed comprehensive CRUD functions using Flutter and SQLite, enabling full functionality of the application.',
+      'Conducted rigorous testing and user feedback analysis, identifying and eliminating bugs to enhance user experience.'
     ],
   },
   {
     name: "GreenWaste",
     description:
-      "Project to foster waste sorting through a reward system and so to contribute caring for the environment. The project makes up itself that a web application for administrators to manage users, materials to recycle and rewards, in addition to mobile application for users can request containers and redeem rewards.",
+      "Project to foster waste sorting through a reward system and contribute to caring for the environment. The project consists of a web application for administrators to manage users, materials to recycle, and rewards, in addition to being a mobile application where users can request containers and redeem rewards.",
     tags: [
       {
         name: "Flutter",
@@ -282,17 +282,17 @@ const projects = [
     title: "Full Stack Mobile Developer",
     date: "July 2022 - August 2022",
     points: [
-      'Collaborated with a team of 5 to design and understand user stories.',
-      'Collaborated with work team to design database model.',
-      'Collaborated with work team to develop 100% Restful API to control user functions using Spark with MySQL as a persistence layer while utilizing Vue.js on the web app and Flutter on the mobile app.',
-      'Collaborated with a team of 5 to design and develop datamining algorithm to analyze rewards the most popular.',
-      'Participated in code reviews with work team, and gained feedback from leader and experience in writing code.'
+      'Collaborated with a team of 5 to design and understand user stories, ensuring clear and actionable requirements.',
+      'Worked with the team to design the database model, optimizing data storage and retrieval.',
+      'Developed a fully RESTful API to control user functions using Spark and MySQL as a persistence layer, while utilizing Vue.jsfor the web app and Flutter for the mobile app.',
+      'Collaborated with team to design and develop a datamining algorithm, analyzing the most popular rewards.',
+      'Participated in code reviews with the team, receiving feedback from the leader and gaining experience in writing high-quality code.'
     ],
   },
   {
     name: "QuickBreak",
     description:
-      "A mobile application inspired in the Pomodoro Technique but focused for videogame players. Its goal is to prevent eye strain by using an alarm that remembers users when to take a break.",
+      "A mobile application inspired by the Pomodoro Technique, but intended for videogame players. Its goal is to prevent eye strain by using an alarm that reminds users when to take a break.",
     tags: [
       {
         name: "Flutter",
@@ -314,18 +314,18 @@ const projects = [
     title: "Full Stack Mobile Developer",
     date: "June 2022 - August 2022",
     points: [
-      'Designed database model.',
-      'Developed an easy-to-understand-use interfaces.',
-      'Designed and understood user stories.',
-      'Developed 100% Restful API to control user functions using Express.Js With MongoDB as a persistence layer while utilizing Flutter on the frontend.',
-      'Implemeted fingerprint and face detetion sensors.',
-      'Deployed database on MongoDB Atlas and backend on Heroku.'
+      'Designed the database model, ensuring efficient data organization and retrieval.',
+      'Developed easy-to-understand user interfaces, enhancing usability and user experience.',
+      'Understood and refined user stories, ensuring clear and actionable requirements.',
+      'Developed a fully RESTful API to control user functions using Express.jsand MongoDB as a persistence layer, while utilizing Flutter for the frontend.',
+      'Implemented fingerprint and face detection sensors, enhancing security and user authentication.',
+      'Deployed the database on MongoDB Atlas and the backend on Heroku, ensuring reliable hosting and scalability.'
     ],
   },
   {
     name: "UnsafeZone",
     description:
-      "Web application to provide information about insecurity level of a public place. The users can register their experience with comments, images and rating a place from 1 to 10 in agreement that perceived unsafe based on what they perceived unsafe.",
+      "Web application to provide information about insecurity level of a public place. The users can register their experience with comments, images and rating from 1 to 10 based on what is perceived to be unsafe.",
     tags: [
       {
         name: "Angular",
@@ -349,16 +349,16 @@ const projects = [
     title: "Full Stack Web Developer",
     date: "October 2021 - December 2021",
     points: [
-      'Designed and understood user stories.',
-      'Designed data models using best practices to ensure high data quality and reduced redundancy.',
-      "Developed atractive and friendly user interfaces using Figma, enabling customers to understand app's operation.",
-      'Developed UnsafeZone with technology like Express and MongoDB to manage and pass requests from Angular front-end.'
+      'Understood and designed user stories, ensuring clear and actionable requirements.',
+      'Designed data models using best practices, ensuring high data quality and reduced redundancy.',
+      "Developed attractive and user-friendly interfaces using Figma, enabling customers to easily understand the app's operation.",
+      'Developed UnsafeZone, a web application, using Express and MongoDB for the backend API and Angular for the front-end.'
     ],
   },
   {
     name: "TophyGalaxy",
     description:
-      "Web application to provide news, reviews and articles about entertainment media like movies or series. The web app allows users to write their reviews and leave comments, providing simply and user-friendly interface.",
+      "Web application providing simple news, reviews and articles about entertainment media like movies or series. The web app allows users to write their reviews and leave comments, providing simply and user-friendly interface.",
     tags: [
       {
         name: "PHP",
@@ -379,10 +379,10 @@ const projects = [
     title: "Full Stack Web Developer",
     date: "November 2020 - December 2020",
     points: [
-      'Collaborated with a pair team to design and understood user stories.',
-      'Collaborated with pair team to concisely design database model.',
-      'Developed atractive, useful and  friendly interfaces using draw.io, enabling users to easily find wishful information.',
-      'Developed a 100% Restful API to control user functions using CodeIgniter with MySQL.'
+      'Collaborated with a team to design and understand user stories, ensuring clear and actionable requirements.',
+      'Worked with the team to design an efficient database model, optimizing data storage and retrieval.',
+      'Developed attractive, useful, and user-friendly interfaces using draw.io, enabling users to easily find the desired information.',
+      'Developed a fully RESTful API to control user functions using CodeIgniter and MySQL, ensuring efficient and reliable backend functionality.'
     ],
   },
 ];
